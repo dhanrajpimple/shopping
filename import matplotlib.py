8c1b1bad9391c9ad602d5d11c52dfe7a2410447b
dhanraj <dhanrajpimple16@gmail.com>
2023-06-03 22:23:38 +0530

    it's dp

---
+++ import matplotlib.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+xpoints = np.array([2, 9])
+ypoints = np.array([5, 15])
+plt.plot(xpoints, ypoints)
+plt.show()
---
+++ script2.js
@@ -0,0 +1,8 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', () => {
+  const query = searchInput.value;
+  Perform search using query
+  Display search results
+});
